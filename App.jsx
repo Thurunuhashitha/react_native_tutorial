f22b15dde25ad71f6e6fc280a6400b2f2008e8ff
Thurunu Hashitha <106402728+Thurunuhashitha@users.noreply.github.com>
2026-02-23 15:04:59 +0530

    creat drawer navigation

--- App.jsx
+++ App.jsx
@@ -17,6 +17,9 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
+import { createDrawerNavigator } from '@react-navigation/drawer';
+const Drawer = createDrawerNavigator();
+
 function App() {
 
   return (
@@ -30,68 +33,82 @@ function App() {
     //   <Stack.Screen name="getall" component={Getall} />
     // </Stack.Navigator>
     // </NavigationContainer>
-    <NavigationContainer> 
-      <Tab.Navigator> 
-        <Tab.Screen
-          name="Register"
-          component={Register}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="account-plus" color={color} size={size} />
-            ),
-          }}
-        />
 
-        <Tab.Screen
-          name="Login"
-          component={Login}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="login" color={color} size={size} />
-            ),
-          }}
-        />
 
-        <Tab.Screen
-          name="Save"
-          component={Save}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="content-save-outline" color={color} size={size} />
-            ),
-          }}
-        />
 
-        <Tab.Screen
-          name="Delete"
-          component={Delete}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="account-remove-outline" color={color} size={size} />
-            ),
-          }}
-        />
+    // <NavigationContainer> 
+    //   <Tab.Navigator> 
+    //     <Tab.Screen
+    //       name="Register"
+    //       component={Register}
+    //       options={{
+    //         tabBarIcon: ({ color, size }) => (
+    //           <MaterialCommunityIcons name="account-plus" color={color} size={size} />
+    //         ),
+    //       }}
+    //     />
 
-        <Tab.Screen
-          name="Update"
-          component={Update}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="account-edit-outline" color={color} size={size} />
-            ),
-          }}
-        />
+    //     <Tab.Screen
+    //       name="Login"
+    //       component={Login}
+    //       options={{
+    //         tabBarIcon: ({ color, size }) => (
+    //           <MaterialCommunityIcons name="login" color={color} size={size} />
+    //         ),
+    //       }}
+    //     />
 
-        <Tab.Screen
-          name="Get All"
-          component={Getall}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="account-group-outline" color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+    //     <Tab.Screen
+    //       name="Save"
+    //       component={Save}
+    //       options={{
+    //         tabBarIcon: ({ color, size }) => (
+    //           <MaterialCommunityIcons name="content-save-outline" color={color} size={size} />
+    //         ),
+    //       }}
+    //     />
+
+    //     <Tab.Screen
+    //       name="Delete"
+    //       component={Delete}
+    //       options={{
+    //         tabBarIcon: ({ color, size }) => (
+    //           <MaterialCommunityIcons name="account-remove-outline" color={color} size={size} />
+    //         ),
+    //       }}
+    //     />
+
+    //     <Tab.Screen
+    //       name="Update"
+    //       component={Update}
+    //       options={{
+    //         tabBarIcon: ({ color, size }) => (
+    //           <MaterialCommunityIcons name="account-edit-outline" color={color} size={size} />
+    //         ),
+    //       }}
+    //     />
+
+    //     <Tab.Screen
+    //       name="Get All"
+    //       component={Getall}
+    //       options={{
+    //         tabBarIcon: ({ color, size }) => (
+    //           <MaterialCommunityIcons name="account-group-outline" color={color} size={size} />
+    //         ),
+    //       }}
+    //     />
+    //   </Tab.Navigator>
+    // </NavigationContainer>
+
+    <NavigationContainer>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Register" component={Register} />
+      <Drawer.Screen name="Login" component={Login} />
+      <Drawer.Screen name="Save" component={Save} />
+      <Drawer.Screen name="Delete" component={Delete} />
+      <Drawer.Screen name="Update" component={Update} />
+      <Drawer.Screen name="Get All" component={Getall} />
+    </Drawer.Navigator>
     </NavigationContainer>
   );
 }  
